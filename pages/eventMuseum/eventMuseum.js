1748c697040929af50ea0e5761c2e539bcdbd237
ms <953096410@qq.com>
2018-11-07 18:20:47 +0800

    博物馆修改

--- pages/eventMuseum/eventMuseum.js
+++ pages/eventMuseum/eventMuseum.js
@@ -1,7 +1,6 @@
 // pages/eventMuseum/eventMuseum.js
 const app = getApp()
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -50,6 +49,63 @@ Page({
       }
     })
   },
+  
+  onLoad: function () {
+    // this.resetData();
+    // this.loading();
+    // this.getTypeData(app.page);
+  },
+  // getTypeData: function (page) {
+  //   wx.request({
+  //     url: 'http://v.juhe.cn/toutiao/index?type=' + app.requestUrl + '&key=1c84600b999ae65c986571e77b403fab',
+  //     method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+  //     // header: {}, // 设置请求的 header
+  //     success: (res) => {
+  //       // success
+  //         console.log( res.data.result.data );
+  //       var data = res.data.result.data;
+  //       if(data){
+  //         for (var i = page; i < page + 5; i++) {
+  //           app.title.push(data[i].title);
+  //           app.imgUrls.push(data[i].thumbnail_pic_s);
+  //           app.author.push(data[i].author_name);
+  //           app.date.push(data[i].date);
+  //           app.url.push(data[i].url);
+  //         }
+  //         //   console.log( app.imgUrls );
+  //         this.setData({
+  //           title: app.title,
+  //           imgUrls: app.imgUrls,
+  //           author: app.author,
+  //           date: app.date,
+  //           page: app.page
+  //         })
+  //       }
+  //     },
+  //     fail: function () {
+  //       // fail
+  //     },
+  //     complete: function () {
+  //       // complete
+  //       wx.hideToast();
+  //     }
+  //   })
+  // },
+  // resetData: function () {
+  //   app.title = [];
+  //   app.imgUrls = [];
+  //   app.author = [];
+  //   app.title = [];
+  //   app.date = [];
+  //   app.page = 0;
+  // },
+  loading: function () {
+    wx.showToast({
+      title: '加载中',
+      icon: 'loading',
+      duration: 10000
+    })
+  },
   pullDownRefresh: function (e) {
     console.log("下拉刷新");
     this.onLoad();
@@ -61,14 +117,14 @@ Page({
     })
     console.log("上拉加载" + app.page);
     this.loading();
-    this.getTypeData(app.page);
+    // this.getTypeData(app.page);
   },
   check: function (e) {
     // console.log(e.target.dataset.id);
     // console.log(e.target.id);
     app.cssActive = e.target.dataset.id
     app.requestUrl = e.target.id;
-    this.resetData();
+    // this.resetData();
     this.setData({
       dataId: app.cssActive,
       title: app.title,
@@ -79,63 +135,7 @@ Page({
       page: app.page
     }),
       this.loading();
-    this.getTypeData(app.page);
-  },
-  onLoad: function () {
-    this.resetData();
-    this.loading();
-    this.getTypeData(app.page);
-  },
-  getTypeData: function (page) {
-    wx.request({
-      url: 'http://v.juhe.cn/toutiao/index?type=' + app.requestUrl + '&key=1c84600b999ae65c986571e77b403fab',
-      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-      // header: {}, // 设置请求的 header
-      success: (res) => {
-        // success
-          console.log( res.data.result.data );
-        var data = res.data.result.data;
-        if(data){
-          for (var i = page; i < page + 5; i++) {
-            app.title.push(data[i].title);
-            app.imgUrls.push(data[i].thumbnail_pic_s);
-            app.author.push(data[i].author_name);
-            app.date.push(data[i].date);
-            app.url.push(data[i].url);
-          }
-          //   console.log( app.imgUrls );
-          this.setData({
-            title: app.title,
-            imgUrls: app.imgUrls,
-            author: app.author,
-            date: app.date,
-            page: app.page
-          })
-        }
-      },
-      fail: function () {
-        // fail
-      },
-      complete: function () {
-        // complete
-        wx.hideToast();
-      }
-    })
-  },
-  resetData: function () {
-    app.title = [];
-    app.imgUrls = [];
-    app.author = [];
-    app.title = [];
-    app.date = [];
-    app.page = 0;
-  },
-  loading: function () {
-    wx.showToast({
-      title: '加载中',
-      icon: 'loading',
-      duration: 10000
-    })
+    // this.getTypeData(app.page);
   },
   /**
    * 生命周期函数--监听页面隐藏
